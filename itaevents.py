fea5af99108c1b9ae036716b7183fb453dea0f82
posa94 <giacomoposanzini@gmail.com>
2025-05-19 14:23:13 +0200

    Update itaevents.py

--- itaevents.py
+++ itaevents.py
@@ -13,8 +13,8 @@ import re
 from urllib.parse import quote_plus  # Add this import
 
 # Constants
-LINKMFP = "https://tuonome.hf.space"
-PASSWORD = "tuapassword"
+LINKMFP = "https://huggingface.co/spaces/posa94"
+PASSWORD = "Nicholas24"
 
 PROXY = f"{LINKMFP}/proxy/hls/manifest.m3u8?api_password={PASSWORD}&d="
 
